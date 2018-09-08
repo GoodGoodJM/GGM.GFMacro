664107af9e3b8ce71f01ed91c51b8c9ce6a4293c
junmoo heo <gjwnsan9420@gmail.com>
2018-09-09 01:21:33 +0900

    Add Momohandler.

---
+++ GGM.GFMarcro.Core/Handler/MomoHandler.cs
@@ -0,0 +1,32 @@
+﻿using GGM.GFMarcro.Core.Handler.Exception;
+using System;
+using System.Drawing;
+
+namespace GGM.GFMarcro.Core.Handler
+{
+    // Warning : This class is not thread-safe.
+    public class MomoHandler : BaseApplicationHandler
+    {
+        private const string LD_PLAYER_MAIN_FRAME = "LDPlayerMainFrame";
+        private const string RENDER_WINDOW = "RenderWindow";
+        private const string THE_RENDER = "TheRender";
+
+        public MomoHandler(string applicationName)
+        {
+            ApplicationName = applicationName;
+
+            ApplicationWindow = Kernel.User32.FindWindow(LD_PLAYER_MAIN_FRAME, applicationName);
+            if (ApplicationWindow == IntPtr.Zero)
+                throw new ApplicationHandlerException(ApplicationHandlerExceptionType.CANNOT_FIND_APPLICATION);
+
+            var renderHandler = Kernel.User32.FindWindowEx(ApplicationWindow, IntPtr.Zero, RENDER_WINDOW, THE_RENDER);
+            if (renderHandler == IntPtr.Zero)
+                throw new ApplicationHandlerException(ApplicationHandlerExceptionType.CANNOT_FIND_APPLICATION);
+            TargetHandle = renderHandler;
+        }
+
+        public override string ApplicationName { get; }
+        public override IntPtr ApplicationWindow { get; }
+        public override IntPtr TargetHandle { get; }
+    }
+}
